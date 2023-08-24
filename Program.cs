64ef8f48c5cdf4493b1fe9ddca1a56999621a06b
Jacques Adams <adamsjacques546@gmail.com>
2023-08-24 12:39:03 -0400

    added logging commit

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ using Students.Api.Settings;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<MongoDbSettings>();
 builder.Services.AddSingleton<IStudentsRepository,StudentsRepository>();
-var app = builder.Build();
 
+var app = builder.Build();
+app.UseHttpLogging();
 app.MapGet("/", () => "Hello World!");
 app.MapStudentsEndpoints();
 app.Run();
